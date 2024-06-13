55d56ab35fec5a682599822092055dfa90a5e5ca
suresh <lavanyagundu3@gmail.com>
2024-06-13 15:39:03 +0530

    new basicauth.java

--- basicauth.java
+++ basicauth.java
@@ -1,3 +1,4 @@
+
 import java.net.URI;
 import java.util.function.Predicate;
 
@@ -17,6 +18,8 @@ public class basicauth {
 		WebDriver driver = new ChromeDriver(options);
 		/home/lavanya/pushdirectory/project/uifrontend/ptests/basicauth.java
 		Predicate<URI> uriPredicate = uri -> uri.getHost().contains("httpbin.org");
+                 Predicate<URI> uriPredicate = uri -> uri.getHost().contains("httpbin.org");
+
 
 		((HasAuthentication) driver).register(uriPredicate, UsernameAndPassword.of("foo", "bar"));
 		driver.get("http://httpbin.org/basic-auth/foo/bar");
